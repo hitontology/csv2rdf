4a2f68a5ccc0ff57cfb3f57f3d00d75f61b17fba
ThomasPause <tp55sude@studserv.uni-leipzig.de>
2020-04-24 15:42:41 +0200

    change classlist.py to output as md table rows

--- classlist.py
+++ classlist.py
@@ -21,12 +21,12 @@ for resource in stdin:
 
 #print(classes)
 sorted = sorted(classes.keys(), key=lambda k: -len(classes[k]))
-sorted = sorted[:10]
+sorted = sorted[:11]
 for k in sorted:
     quote = urllib.parse.quote_plus(k)
     query = "https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+COUNT%28DISTINCT%28%3Fx%29%29+%7B%3Fx+a+%3C"+quote+"%3E+%7D&format=text%2Ftab-separated-values&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+"
     size = int(requests.get(query).text.split("\n")[1])
-    print(k,len(classes[k]),size,len(classes[k])*100/size)
-    #print(k,len(classes[k]),size,classes[k])
+    print("|",k,"|",len(classes[k]),"|",size,"|",len(classes[k])*100/size,"|")
+    #print(k+";"+len(classes[k])+";"+size+";"+classes[k])
 print(count,"resources in total")
 
